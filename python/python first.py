c883c319c3e27c12fcb2d5aa6c98d5fe1cbcdbde
soumil420 <valorantaddict420@gmail.com>
2023-04-27 18:49:19 +0530

    school test 27/4/23

--- python/python first.py
+++ python/python first.py
@@ -1,3 +1,6 @@
 print (1,2,3,4,5,6, sep="||", end="/t !!")
 #comment
-print ("Hello\nWorld,\nI am happy to help\'No I am not happy\v i hope you enjoy yourself\a")
+print ("lolololol\rHello\nWorld,\nI am happy to help\'No\' I am not happy\t i hope you enjoy yourself\b")
+a=50.25
+b=int(a)
+print (b) #this is explicit type conversion
