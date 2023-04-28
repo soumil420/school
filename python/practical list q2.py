b9613c93ddd84adbf802459584d893ff4e474fe4
aismvsoumil <132035669+aismvsoumil@users.noreply.github.com>
2023-04-28 10:00:18 +0530

    Add files via upload

---
+++ python/practical list q2.py
@@ -0,0 +1,12 @@
+x=float(input("Enter first number: "))
+y=float(input("Enter second number: "))
+z=str(input"Enter operator")
+
+if z=="+":
+    print(x+y)
+elif z=="-":
+    print(x-y)
+elif z=="*":
+    print(x*y)
+elif z=="/":
+    print(x/y)
